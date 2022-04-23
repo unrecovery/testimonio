edc3890f1ec9145df9ecc138e5fd3bc16365f71b
Kevin Gabriel Rodriguez <53584268+unrecovery@users.noreply.github.com>
2022-04-23 12:52:36 -0500

    Update js.js

--- js/js.js
+++ js/js.js
@@ -2,19 +2,19 @@ const testimonio = [
   {
     name: "LordMegatron",
     photoUrl:
-      "https://scontent.fvsa1-1.fna.fbcdn.net/v/t39.30808-6/240513106_4291951554200596_1683310416178300657_n.jpg?_nc_cat=105&ccb=1-5&_nc_sid=09cbfe&_nc_eui2=AeGQ9YHVm61PDozNUNNFImotqIluFrpIisuoiW4WukiKy_Zo_nKNwPluZzSQxXcRYfk-2nnyaCGNBOq9qSMrQ6x7&_nc_ohc=c2Bx1nFDEYIAX-0w2r1&tn=lA-_dOb-ntJjFlNJ&_nc_ht=scontent.fvsa1-1.fna&oh=b3a139947897b36706f61ad55315607e&oe=61A2C9DE",
+      "https://scontent.fvsa1-2.fna.fbcdn.net/v/t39.30808-6/240513106_4291951554200596_1683310416178300657_n.jpg?_nc_cat=105&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=nvwmYr_bqwkAX_fc_BH&_nc_ht=scontent.fvsa1-2.fna&oh=00_AT9uSVToHDFQWzQcmTUzXkr4jEPG54n2PKOu6DCfS1bpwA&oe=62688ADE",
     text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam amet sint ipsam, consectetur exercitationem velit quod beatae recusandae adipisci, id neque dicta necessitatibus soluta, laudantium alias sapiente libero tempora facilis?.",
   },
   {
-    name: "Winning",
+    name: "Boyka",
     photoUrl:
-      "https://scontent.fvsa1-1.fna.fbcdn.net/v/t39.30808-6/244142150_2894135747564042_5393753293837592828_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=09cbfe&_nc_eui2=AeHvMmGXUCQqi66hwMp2T8rSp-dc8SPa5Ran51zxI9rlFpPrDpkPwjh8LmbCCROk8tLVp0eb_hi5rGlOEgFNztOm&_nc_ohc=dQIhihWz-NkAX_jS9ub&_nc_ht=scontent.fvsa1-1.fna&oh=a230b09d91d6f5aa1db97d781fb090dc&oe=61A29AF2",
+      "https://scontent.fvsa1-2.fna.fbcdn.net/v/t39.30808-6/273528266_1364766540618898_2034142996575194905_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=s5-kemPsDb4AX9YZT-U&_nc_ht=scontent.fvsa1-2.fna&oh=00_AT9iiGVRegSwokbAtcV1hMBI6gO0fQGKncL4YUPeeIVtdQ&oe=62691642",
     text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam amet sint ipsam, consectetur exercitationem velit quod beatae recusandae adipisci, id neque dicta necessitatibus soluta, laudantium alias sapiente libero tempora facilis?.",
   },
   {
     name: "Danger",
     photoUrl:
-      "https://scontent.fvsa1-1.fna.fbcdn.net/v/t1.6435-9/60795662_1545059828962842_1863815618038333440_n.jpg?_nc_cat=111&ccb=1-5&_nc_sid=09cbfe&_nc_eui2=AeGu0SMCmNpO5iteN3_EfBu24ob1XV2NA0bihvVdXY0DRqddeZ-QDWtqWVDj3V55CUujHgEZ5jZCnMhU70aoz_oT&_nc_ohc=C030TUKx9BoAX-mRsVo&tn=XlEewdt2ZPmoDndI&_nc_ht=scontent.fvsa1-1.fna&oh=b831cadbf2a7f730e1640acf9a772c09&oe=61C13142",
+      "https://scontent.fvsa1-1.fna.fbcdn.net/v/t39.30808-6/275521983_3008732149437734_166831737385082443_n.jpg?_nc_cat=106&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=hszO6qMks14AX-sVepM&tn=ES6fJXEBob0gTdK6&_nc_ht=scontent.fvsa1-1.fna&oh=00_AT-CyElP47kM51OB02oZXTe4hCUuzNvok0lgT1E-tFO4oQ&oe=626886DB",
     text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam amet sint ipsam, consectetur exercitationem velit quod beatae recusandae adipisci, id neque dicta necessitatibus soluta, laudantium alias sapiente libero tempora facilis?.",
   },
   {
